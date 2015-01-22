bd9a55a891b1dbf5f8f925e81ec78ba38ffe9e4f
otknoy <otknoy@gmail.com>
2015-01-22 13:47:13 +0900

    Change reftex-default-bibliography

--- inits/40-tex.el
+++ inits/40-tex.el
@@ -1,4 +1,4 @@
 ;; reftex-mode
 (add-hook 'yatex-mode-hook 'turn-on-reftex)
 ;(setq reftex-default-bibliography '("~/Dropbox/semi/otsuka_graduatethesis/bib/reference.bib"))
-(setq reftex-default-bibliography '("~/masterthesis/reference.bib"))
+(setq reftex-default-bibliography '("~/masterthesis/masterthesis/reference.bib"))
