839621c5d59b5fe868eb989e13dc2f974a48063e
otknoy <otknoy@gmail.com>
2015-10-18 18:28:02 +0900

    Reconfigure python settings

--- inits/40-python.el
+++ inits/40-python.el
@@ -1,22 +1,3 @@
-; jedi
-(require 'jedi)
-(setenv "PYTHONPATH" "/Users/otknoy/.pyenv/versions/3.4.3/lib/python3.4/site-packages/")
-
+;;; jedi
 (add-hook 'python-mode-hook 'jedi:setup)
 (setq jedi:complete-on-dot t)
-
-; flymake
-(add-hook 'python-mode-hook
-          '(lambda ()
-             (flymake-mode t)))
-(load-library "flymake-cursor.el")
-(when (load "flymake" t)
-  (defun flymake-pylint-init ()
-    (let* ((temp-file (flymake-init-create-temp-buffer-copy
-                       'flymake-create-temp-inplace))
-           (local-file (file-relative-name
-                        temp-file
-                        (file-name-directory buffer-file-name))))
-      (list "pycheckers" (list local-file))))
-  (add-to-list 'flymake-allowed-file-name-masks
-               '("\\.py\\'" flymake-pylint-init)))
