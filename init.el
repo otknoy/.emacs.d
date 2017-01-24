fe2b17e4cd5cb7eef4cbc17088b973bffaab315e
otknoy <otknoy@gmail.com>
2017-01-25 00:50:51 +0900

    Use package.el

--- init.el
+++ init.el
@@ -1,24 +1,11 @@
+(require 'package)
 
-;; Added by Package.el.  This must come before configurations of
-;; installed packages.  Don't delete this line.  If you don't want it,
-;; just comment it out by adding a semicolon to the start of the line.
-;; You may delete these explanatory comments.
-; (package-initialize)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
 
-(when load-file-name
-  (setq user-emacs-directory (file-name-directory load-file-name)))
+(package-initialize)
+(package-refresh-contents)
 
-;; el-get
-(add-to-list 'load-path "~/.emacs.d/el-get/el-get")
-(unless (require 'el-get nil 'noerror)
-  (with-current-buffer
-      (url-retrieve-synchronously
-       "https://raw.githubusercontent.com/dimitri/el-get/master/el-get-install.el")
-    (goto-char (point-max))
-    (eval-print-last-sexp)))
-(add-to-list 'el-get-recipe-path "~/.emacs.d/el-get-user/recipes")
-(el-get 'sync)
-
+(package-install 'init-loader)
 
 (load-file "~/.emacs.d/elisp/packages.el")
 
@@ -35,4 +22,3 @@
 (load-file "~/.emacs.d/elisp/web.el")
 
 (load-file "~/.emacs.d/elisp/others.el")
-
