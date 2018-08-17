0a97aefd8025fe9aa2485503b0d09192f929339a
Naoya Otsuka <otknoy@MacBook.local>
2018-08-17 22:40:19 +0900

    Add rjsx-mode

--- elisp/others.el
+++ elisp/others.el
@@ -33,3 +33,12 @@
 (setq-default indent-tabs-mode nil)
 (setq-default tab-width 2)
 (setq-default js2-basic-offset 2)
+
+;; rjsx-mode
+(add-to-list 'auto-mode-alist '(".*\\.js\\'" . rjsx-mode))
+(add-hook 'rjsx-mode-hook
+          (lambda ()
+            (setq indent-tabs-mode nil)
+            (setq js-indent-level 2)
+            )
+          )
