15844886e52a20dd4c83aba64e25403ba6e7f1e4
Naoya Otsuka <otknoy@gmail.com>
2019-03-02 13:23:33 +0900

    auto-complete-mode -> company-mode

--- conf/util.el
+++ conf/util.el
@@ -7,27 +7,12 @@
     (global-auto-highlight-symbol-mode t)
     (ahs-set-idle-interval 0.3)))
 
-
-(use-package auto-complete
+(use-package company
   :config
   (progn
-    (require 'auto-complete-config)
-    (ac-config-default)
-
-    (global-auto-complete-mode t)
-
-    (add-to-list 'ac-dictionary-directories
-		 "~/.emacs.d/elisp/auto-complete/ac-dict")
-    (setq ac-comphist-file "~/.emacs.d/elisp/auto-complete/ac-comphist.dat")
-
-    ;;; keybind
-    (setq ac-use-menu-map t)
-    (define-key ac-menu-map "\C-n" 'ac-next)
-    (define-key ac-menu-map "\C-p" 'ac-previous)
-
-    ;; キーは好きなのを割り当てて下さい
-    (global-set-key (kbd "M-h") 'ac-complete-look)
-    ))
+    (add-hook 'after-init-hook 'global-company-mode)
+    )
+  )
 
 (use-package flycheck
   :straight t
