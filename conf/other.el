40ee6a4abb9c77974aa64d4c4e7e52d6bb1b368c
Naoya Otsuka <otknoy@gmail.com>
2019-03-03 14:13:06 +0900

    organize file structure

--- conf/other.el
+++ conf/other.el
@@ -1,3 +1,16 @@
+(use-package eldoc
+  :init
+  (add-hook 'emacs-lisp-mode-hook 'eldoc-mode)
+  (add-hook 'lisp-interaction-mode-hook 'eldoc-mode)
+  (add-hook 'ielm-mode-hook 'eldoc-mode)
+  :config
+  (setq eldoc-idle-delay 0.2)
+  (setq eldoc-echo-area-use-multiline-p t)
+  (turn-on-eldoc-mode)
+)
+
+(use-package dockerfile-mode)
+
 (use-package markdown-mode
   :mode (("\\.\\(txt\\|md\\)$" . markdown-mode))
   :config
---
+++ conf/view.el
@@ -0,0 +1,58 @@
+;;; color-theme
+(use-package color-theme
+  :if window-system
+  :config
+  (progn
+    (color-theme-initialize)
+    (color-theme-clarity))
+  )
+
+;;; color setting
+(when window-system
+  ;; menu, tool and scroll bar are invisible.
+  (menu-bar-mode 0)
+  (tool-bar-mode 0)
+  (scroll-bar-mode 0)
+  ;; frame alphan
+  (set-frame-parameter nil 'alpha 80))
+
+;;; font settings
+;; Mac OSX
+(when (eq system-type 'darwin)
+  (set-face-attribute 'default nil
+		      :family "Ricty"
+		      :height 135)
+  ;; Japanese font
+  (set-fontset-font
+   nil 'japanese-jisx0208
+   (font-spec :family "Ricty")))
+;; linux
+(when (eq system-type 'gnu/linux)
+  (set-face-attribute 'default nil
+		      :family "Ricty"
+		      :height 135))
+;; windows
+(when (eq system-type 'windows-nt)
+  (set-face-attribute 'default nil
+		      :family "Ricty Ascii"
+		      :height 130)
+  ;; Japanese font
+  (set-fontset-font
+   nil 'japanese-jisx0208
+   (font-spec :family "Meiryo" :size 13)))
+
+
+;;; line highlight
+(defface hlline-face
+  '((((class color)
+      (background dark))
+     (:background "dark slate gray"))
+    (((class color)
+      (background light))
+     (:background ""))
+    (t
+     ()))
+  "*Face used by hl-line.")
+(setq hl-line-face 'hlline-face)
+;; (setq hl-line-face 'underline)
+(global-hl-line-mode)
