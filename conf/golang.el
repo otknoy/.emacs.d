4627d28bd5c1ad5e884d5609f2151d40b95a5fbe
Naoya Otsuka <otknoy@gmail.com>
2020-07-04 11:34:04 +0900

    simplify golang settings

--- conf/golang.el
+++ conf/golang.el
@@ -1,10 +1,6 @@
 (use-package go-mode
   :init
   (add-hook 'before-save-hook 'gofmt-before-save)
-  (add-hook
-   'go-mode-hook
-   '(lambda () (setq tab-width 2))
-   )
   (add-hook 'go-mode-hook 'flycheck-mode)
   (use-package go-eldoc
     :hook
@@ -14,18 +10,6 @@
 			:underline t :foreground "green"
 			:weight 'bold)
     )
-
-  :config
-  (when (equal system-type 'gnu/linux)
-    (setq homedir "/home/otknoy"))
-  (when (equal system-type 'darwin)
-    (setq homedir "/Users/otknoy"))
-  (setq gopath (concat homedir "/go"))
-  (setenv "GOPATH" gopath)
-  (add-to-list 'exec-path (expand-file-name (concat gopath "/bin")))
-
-  ;; goimports
-  (setq gofmt-command "goimports")
   )
 
 (use-package company
