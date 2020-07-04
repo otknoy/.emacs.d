7806ad34830a482fa63828a523ff64fbdb1f7973
Naoya Otsuka <otknoy@gmail.com>
2020-07-04 12:02:11 +0900

    modify tab width in go-mode

--- conf/golang.el
+++ conf/golang.el
@@ -1,6 +1,8 @@
 (use-package go-mode
   :init
   (add-hook 'before-save-hook 'gofmt-before-save)
+  (add-hook 'go-mode-hook
+	    '(lambda () (setq tab-width 2)))
   (add-hook 'go-mode-hook 'flycheck-mode)
   (use-package go-eldoc
     :hook
