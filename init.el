2c216f9f5203ad8e715ae5afb53ea267023989c8
Naoya Otsuka <otknoy@gmail.com>
2020-08-29 13:56:57 +0900

    leaf.el install code

--- init.el
+++ init.el
@@ -30,3 +30,34 @@
 (load-file "~/.emacs.d/conf/web.el")
 (load-file "~/.emacs.d/conf/other.el")
 (load-file "~/.emacs.d/conf/lsp.el")
+
+
+;; <leaf-install-code>
+(eval-and-compile
+  (when (or load-file-name byte-compile-current-file)
+    (setq user-emacs-directory
+          (expand-file-name
+           (file-name-directory (or load-file-name byte-compile-current-file))))))
+
+(eval-and-compile
+  (customize-set-variable
+   'package-archives '(("org" . "https://orgmode.org/elpa/")
+                       ("melpa" . "https://melpa.org/packages/")
+                       ("gnu" . "https://elpa.gnu.org/packages/")))
+  (package-initialize)
+  (unless (package-installed-p 'leaf)
+    (package-refresh-contents)
+    (package-install 'leaf))
+
+  (leaf leaf-keywords
+    :ensure t
+    :init
+    ;; optional packages if you want to use :hydra, :el-get, :blackout,,,
+    (leaf hydra :ensure t)
+    (leaf el-get :ensure t)
+    (leaf blackout :ensure t)
+
+    :config
+    ;; initialize leaf-keywords.el
+    (leaf-keywords-init)))
+;; </leaf-install-code>
