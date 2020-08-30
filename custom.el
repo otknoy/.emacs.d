1931a0f67b7908b429b6298e0a7c6e7771e83f79
Naoya Otsuka <otknoy@gmail.com>
2020-08-30 12:19:12 +0900

    add custom.el

---
+++ custom.el
@@ -0,0 +1,32 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(auto-revert-check-vc-info t)
+ '(auto-revert-interval 0.3)
+ '(auto-save-file-name-transforms (quote ((".*" "~/.emacs.d/backup/" t))))
+ '(auto-save-interval 60)
+ '(auto-save-timeout 15)
+ '(backup-directory-alist (quote ((".*" . "~/.emacs.d/backup") ("\\`/.+:.*:"))))
+ '(delete-old-versions t)
+ '(imenu-list-position (quote left) t)
+ '(imenu-list-size 30 t)
+ '(package-archives
+   (quote
+    (("org" . "https://orgmode.org/elpa/")
+     ("melpa" . "https://melpa.org/packages/")
+     ("gnu" . "https://elpa.gnu.org/packages/"))))
+ '(package-selected-packages
+   (quote
+    (lsp-ui go-mode magit lsp-mode leaf-tree leaf-convert hydra git-gutter flycheck exec-path-from-shell el-get dockerfile-mode docker-compose-mode dimmer company color-theme-modern blackout auto-highlight-symbol anzu)))
+ '(read-file-name-completion-ignore-case t)
+ '(truncate-lines t)
+ '(truncated-partial-width-window-p 0 t)
+ '(version-control t))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
