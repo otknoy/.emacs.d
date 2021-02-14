c6963035dde21bc320b46bc97a3b3c3c2996024b
Naoya Otsuka <otknoy@gmail.com>
2021-02-14 13:22:56 +0900

    refactor leaf init code

--- init.el
+++ init.el
@@ -6,44 +6,40 @@
 
 ;;; Code:
 
-;; <leaf-install-code>
-(eval-and-compile
-  (when (or load-file-name byte-compile-current-file)
-    (setq user-emacs-directory
-          (expand-file-name
-           (file-name-directory (or load-file-name byte-compile-current-file))))))
+(prog1 "prepare leaf"
+  (prog1 "package"
+    (custom-set-variables
+     '(package-archives '(("org"   . "https://orgmode.org/elpa/")
+                          ("melpa" . "https://melpa.org/packages/")
+                          ("gnu"   . "https://elpa.gnu.org/packages/"))))
+    (package-initialize))
 
-(eval-and-compile
-  (customize-set-variable
-   'package-archives '(("org" . "https://orgmode.org/elpa/")
-                       ("melpa" . "https://melpa.org/packages/")
-                       ("gnu" . "https://elpa.gnu.org/packages/")))
-  (package-initialize)
-  (unless (package-installed-p 'leaf)
-    (package-refresh-contents)
-    (package-install 'leaf))
+  (prog1 "leaf"
+    (unless (package-installed-p 'leaf)
+      (unless (assoc 'leaf package-archive-contents)
+        (package-refresh-contents))
+      (condition-case err
+          (package-install 'leaf)
+        (error
+         (package-refresh-contents)       ; renew local melpa cache if fail
+         (package-install 'leaf))))
 
-  (leaf leaf-keywords
-    :ensure t
-    :init
-    ;; optional packages if you want to use :hydra, :el-get, :blackout,,,
-    (leaf hydra :ensure t)
-    (leaf el-get :ensure t)
-    (leaf blackout :ensure t)
-
-    :config
-    ;; initialize leaf-keywords.el
-    (leaf-keywords-init)))
-;; </leaf-install-code>
-
-(leaf leaf
-  :config
-  (leaf leaf-convert :ensure t)
-  (leaf leaf-tree
-    :ensure t
-    :blackout t
-    :custom ((imenu-list-size . 30)
-             (imenu-list-position . 'left))))
+    (leaf leaf
+      :config
+      (leaf leaf-keywords
+	:ensure t
+	:init
+	(leaf hydra :ensure t)
+	(leaf el-get :ensure t
+	  :custom ((el-get-git-shallow-clone  . t)))
+	(leaf blackout :ensure t)
+	:config (leaf-keywords-init))
+      (leaf leaf-convert :ensure t)
+      (leaf leaf-tree
+	:ensure t
+	:blackout t
+	:custom ((imenu-list-size . 30)
+		 (imenu-list-position . 'left))))))
 
 (leaf system
   :init
