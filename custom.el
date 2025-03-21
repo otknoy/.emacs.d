edb41b61e1287184e03166cf0a34b13e4ad5bd08
Naoya Otsuka <otknoy@gmail.com>
2025-03-21 10:28:19 +0900

    unnecessary from v27.2

--- custom.el
+++ custom.el
@@ -3,8 +3,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   '(platformio-mode go-impl lsp-ui docker-compose-mode dockerfile-mode yaml-mode markdown-mode macrostep web-mode go-mode request dash s editorconfig treemacs projectile dashboard orderless consult marginalia vertico lsp-mode popup google-translate git-gutter magit prettier-js company flycheck which-key anzu highlight-indent-guides auto-highlight-symbol dimmer modus-themes auto-package-update exec-path-from-shell blackout hydra leaf-keywords)))
+ '(package-selected-packages nil)
+ '(package-vc-selected-packages
+   '((gotests :url "https://github.com/damienlevin/GoTests-Emacs"))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
