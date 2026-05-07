0dabae15de617e24905e5e8a04c7b431fe0cf28d
otknoy <otknoy@gmail.com>
2026-05-07 21:45:36 +0900

    good-bye leaf

--- init.el
+++ init.el
@@ -7,32 +7,9 @@
 ;; <leaf-install-code>
 (eval-and-compile
   (customize-set-variable
-   'package-archives '(("org" . "https://orgmode.org/elpa/")
+   'package-archives '(("org"   . "https://orgmode.org/elpa/")
                        ("melpa" . "https://melpa.org/packages/")
-                       ("gnu" . "https://elpa.gnu.org/packages/")))
-  (unless (package-installed-p 'leaf)
-    (package-refresh-contents)
-    (package-install 'leaf))
-
-  (leaf leaf-keywords
-    :ensure t
-    :init
-    ;; optional packages if you want to use :hydra, :el-get, :blackout,,,
-    (leaf hydra :ensure t)
-    (leaf blackout :ensure t)
-
-    :config
-    ;; initialize leaf-keywords.el
-    (leaf-keywords-init)))
-;; </leaf-install-code>
-
-(leaf leaf
-  (leaf leaf-convert :ensure t)
-  (leaf leaf-tree
-    :ensure t
-    :blackout t
-    :custom ((imenu-list-size . 30)
-             (imenu-list-position . 'left))))
+                       ("gnu"   . "https://elpa.gnu.org/packages/"))))
 
 (use-package init-loader
   :ensure t
